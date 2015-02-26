57709b8ddd0178633b97763f2ef5affb9797bce8
takashi sawada <taks@takashi-no-MacBook-Air.local>
2015-02-26 14:19:43 +0900

    Initial Commit

---
+++ MapProjectA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapProjectA
+//
+//  Created by takashi sawada on 2015/02/26.
+//  Copyright (c) 2015年 prototype. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
